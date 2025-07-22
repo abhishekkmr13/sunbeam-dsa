5b435f870f4bb2f06dab42943640d2a4a2b5078c
Abhishek Kumar <abhishek.kumar1309@gmail.com>
2025-07-22 18:39:05 +0530

    first commit

---
+++ dsa/src/main/java/com/self/dsa/App.java
@@ -0,0 +1,13 @@
+package com.self.dsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
